21871e4d3481ee3dde0c5ff6c28a693af9c29a88
alilunga <143321660+alilunga@users.noreply.github.com>
2023-09-05 16:01:38 -0700

    Add files via upload

--- Patrick.py
+++ Patrick.py
@@ -1,2 +1,11 @@
-import streamlit as st
-st.write("Je m'appelle Alain ILUNGA")
+# -------------- SETTINGS --------------
+incomes = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency = "USD"
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+# --------------------------------------
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
