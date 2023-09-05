938b38dd54b4f91095262b36c74adf90a98752d2
alilunga <143321660+alilunga@users.noreply.github.com>
2023-09-05 16:16:04 -0700

    Add files via upload

--- Patrick.py
+++ Patrick.py
@@ -1,13 +1,23 @@
-import streamlit as st
+import calendar  # Core Python Module
+from datetime import datetime  # Core Python Module
+
+import streamlit as st  # pip install streamlit
+from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
+
 
 # -------------- SETTINGS --------------
 incomes = ["Salary", "Blog", "Other Income"]
 expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
 currency = "USD"
-page_title = "Income and Expense Tracker"
+page_title = "Patrick Import"
 page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
 # --------------------------------------
 
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
+
+# --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
+years = [datetime.today().year, datetime.today().year + 1]
+months = list(calendar.month_name[1:])
+
