2ce2fdb10d8668570c1326e695847683bba6224d
alilunga <143321660+alilunga@users.noreply.github.com>
2023-09-20 18:57:54 -0700

    Add files via upload

--- Bailch.py
+++ Bailch.py
@@ -1,13 +1,10 @@
-import calendar  # Core Python Module
-from datetime import datetime  # Core Python Module
-
 import streamlit as st  # pip install streamlit
-import plotly.graph_objects as go  # pip install plotly
+from deta import Deta
+
 
 # -------------- SETTINGS --------------
 vdouanes = ["Numero bulletin de liquidation", "Numero quittance", "Date de paiement", "Position tarifaire", "Unite statique", "Quantite", "Nom du declarant", "Valeur en douane"]
-expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
-currency = "USD"
+currency = "FC"
 page_title = "Bailch Import"
 page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
@@ -16,33 +13,26 @@ layout = "centered"
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
-# --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
-years = [datetime.today().year, datetime.today().year + 1]
-months = list(calendar.month_name[1:])
-
-# --- INPUT & SAVE PERIODS ---
+# --- INPUT & SAVE VALEURS DOUANE ---
 st.header(f"Data Entry in {currency}")
-with st.form("entry_form", clear_on_submit=True):
-#        col1, col2 = st.columns(2)
-#        col1.selectbox("Select Month:", months, key="month")
-#        col2.selectbox("Select Year:", years, key="year")
+with st.form("Valeurs_Douane", clear_on_submit=True):
 
-        "---"
-        with st.expander("Valeur Douane"):
-            for vdouane in vdouanes:
-                st.number_input(f"{vdouane}:", min_value=0, format="%i", step=10, key=vdouane)
-#      with st.expander("Expenses"):
-#            for expense in expenses:
-#                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
-        with st.expander("Comment"):
-            comment = st.text_area("", placeholder="Enter a comment here ...")
-        submitted = st.form_submit_button("Save Data")
+        st.write ("Remplir les valeurs douanieres")
+        nbl = st.number_input("Entrer Numero bulletin de liquidation")
+        nqc = st.number_input("Numero quittance")
+        dtp = st.number_input("Date de paiement")
+        ptf = st.number_input("Position tarifaire")
+        ust = st.number_input("Unite statique")
+        qte = st.number_input("Quantite")
+        ndc = st.text_input("Nom du declarant")
+        vld = st.number_input("Valeur en douane")
+        submitted = st.form_submit_button("Enregistrer")
 
-#        "---"
+# --- DATABASE ---
 
-#if submitted:
-#           period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
-#            incomes = {income: st.session_state[income] for income in incomes}
-#            expenses = {expense: st.session_state[expense] for expense in expenses}
-#            db.insert_period(period, incomes, expenses, comment)
-#           st.success("Data saved!")
+deta = Deta(st.secrets["data_key"])
+db = deta.Base("BSHT")
+
+if submitted:
+    db.put({"Numero bulletin de liquidation":nbl, "Numero quittance":nqc, "Date de paiement":dtp, "Position tarifaire":ptf, "Unite statique":ust, "Quantite":qte, "Nom du declarant":ndc, "Valeur en douane":vld})
+    
