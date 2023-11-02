c879aaf4a71b9bf825ebac8f07dbb1212d12d154
alilunga <143321660+alilunga@users.noreply.github.com>
2023-11-02 16:14:58 -0700

    Update Bailch.py

--- Bailch.py
+++ Bailch.py
@@ -3,9 +3,9 @@ from deta import Deta
 
 
 # -------------- SETTINGS --------------
-vdouanes = ["Numero bulletin de liquidation", "Numero quittance", "Date de paiement", "Position tarifaire", "Unite statique", "Quantite", "Nom du declarant", "Valeur en douane"]
+vdouanes = ["id", "Name", "Last Name"]
 currency = "FC"
-page_title = "Bailch Import"
+page_title = "Bailch test"
 page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
 # --------------------------------------
@@ -14,18 +14,12 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
 # --- INPUT & SAVE VALEURS DOUANE ---
-st.header(f"Data Entry in {currency}")
-with st.form("Valeurs_Douane", clear_on_submit=True):
+with st.form("Identite", clear_on_submit=True):
 
-        st.write ("Remplir les valeurs douanieres")
-        nbl = st.number_input("Entrer Numero bulletin de liquidation")
-        nqc = st.number_input("Numero quittance")
-        dtp = st.number_input("Date de paiement")
-        ptf = st.number_input("Position tarifaire")
-        ust = st.number_input("Unite statique")
-        qte = st.number_input("Quantite")
-        ndc = st.text_input("Nom du declarant")
-        vld = st.number_input("Valeur en douane")
+        st.write ("Remplir")
+        id = st.number_input("Entrer Numero bulletin de liquidation")
+        name = st.number_input("Numero quittance")
+        last = st.number_input("Date de paiement")
         submitted = st.form_submit_button("Enregistrer")
 
 # --- DATABASE ---
@@ -34,5 +28,5 @@ deta = Deta(st.secrets["data_key"])
 db = deta.Base("BSHT")
 
 if submitted:
-    db.put({"Numero bulletin de liquidation":nbl, "Numero quittance":nqc, "Date de paiement":dtp, "Position tarifaire":ptf, "Unite statique":ust, "Quantite":qte, "Nom du declarant":ndc, "Valeur en douane":vld})
+    db.put({"id":id, "name":name, "last":last})
     
